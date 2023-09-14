6ddec6a16d2f81b5040aae64e2e20977847056c7
crisfrndz <114977127+crisfrndz@users.noreply.github.com>
2023-09-14 15:55:18 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,33 @@
+function changeimg1() {
+  var img1 = document.getElementById("img1");
+  img1.src =
+    "https://images.pexels.com/photos/1337382/pexels-photo-1337382.jpeg?auto=compress&cs=tinysrgb&w=600";
+}
+function changeimg2() {
+  var img1 = document.getElementById("img2");
+  img1.src =
+    "https://images.pexels.com/photos/1365795/pexels-photo-1365795.jpeg?auto=compress&cs=tinysrgb&w=600";
+}
+function changeimg3() {
+  var img1 = document.getElementById("img3");
+  img1.src =
+    "https://images.pexels.com/photos/1478442/pexels-photo-1478442.jpeg?auto=compress&cs=tinysrgb&w=600";
+}
+
+function vid1() {
+  var vid = document.getElementById("vid1");
+  vid.src =
+    "https://player.vimeo.com/external/434851730.sd.mp4?s=ba5754f20687691b0a3c0c77565788d23b16fde1&profile_id=164&oauth2_token_id=57447761";
+}
+
+function vid2() {
+  var vid = document.getElementById("vid2");
+  vid.src =
+    "https://player.vimeo.com/external/459295100.sd.mp4?s=6f4b8b542310c7e6105803713d6d772b5d2476a1&profile_id=164&oauth2_token_id=57447761";
+}
+
+function vid3() {
+  var vid = document.getElementById("vid3");
+  vid.src =
+    "https://player.vimeo.com/external/441919357.sd.mp4?s=20fa24b53d0c6a483f68722e2afde56d146f6a11&profile_id=164&oauth2_token_id=57447761";
+}
